86a950dc9b95126179ef423d3c75b4b3cfa1ef62
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-08-01 10:48:08 +0300

    end of V1

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,10 @@ from django.core.paginator import Paginator, EmptyPage
 from django.core.mail import send_mail
 from django.db.models import Count
 
+# search part
+from django.contrib.postgres.search import SearchVector
+from .forms import EmailPostForm, CommentForm, SearchForm
+
 from django.views.decorators.http import require_POST
 
 from taggit.models import Tag
@@ -12,7 +16,8 @@ from .forms import EmailPostForm, CommentForm
 
 # Create your views here.
 
-def post_list(request, tag_slug = None):
+
+def post_list(request, tag_slug=None):
     posts = Post.objects.all()
 
     tag = None
@@ -33,9 +38,11 @@ def post_list(request, tag_slug = None):
     }
     return render(request, 'blog/post/list.html', context)
 
+
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, publish__year=year, publish__month=month, publish__day=day)
-    
+    post = get_object_or_404(
+        Post, slug=post, publish__year=year, publish__month=month, publish__day=day)
+
     # List of active comments for this post
     comments = post.comments.filter(active=True)
     # Form for users to comment
@@ -43,16 +50,19 @@ def post_detail(request, year, month, day, post):
 
     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        'tags')).order_by('-same_tags', '-publish')[:4]
 
-    context =  {
+    context = {
         'post': post,
         'form': form,
         'comments': comments,
         'similar_posts': similar_posts,
     }
     return render(request, 'blog/post/detail.html', context)
+
 
 def post_share(request, post_id):
     # Retrieve post by id
@@ -65,19 +75,21 @@ def post_share(request, post_id):
             # Form fields passed validation
             cd = form.cleaned_data
             # ... send email
-            post_url = request.build_absolute_uri( post.get_absolute_url()) 
+            post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
-            send_mail(subject, message, 'rogertestapplications@gmail.com',[cd['to']])
+            send_mail(subject, message,
+                      'rogertestapplications@gmail.com', [cd['to']])
             sent = True
     else:
         form = EmailPostForm()
-    context =  {
+    context = {
         'post': post,
         'form': form,
         'sent': sent,
     }
     return render(request, 'blog/post/share.html', context)
+
 
 @require_POST
 def post_comment(request, post_id):
@@ -92,7 +104,7 @@ def post_comment(request, post_id):
         comment.post = post
         # Save the comment to the database
         comment.save()
-    
+
     context = {
         'post': post,
         'form': form,
--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -41,6 +42,9 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
 
     'taggit',
+
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
